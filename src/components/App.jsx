a8f1153ba5def3762b6a984e347c8c073efa4468
PlatinumKellerman <platinumm@ukr.net>
2022-06-23 15:48:45 +0300

    Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
+import Profile  from "./Profile/Profile";
+import user from "data/user.json"
+
 export const App = () => {
   return (
     <div>
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
